0e17cc37ac4a2613e39882449d21340971d72f68
Ranasinghe TKSA <sachin.ranasinghe1998.13@gmail.com>
2022-04-15 13:57:09 +0530

    cart item component jsx page tables are updated

--- client-app/src/components/component-tksa/CartItemsComponents.jsx
+++ client-app/src/components/component-tksa/CartItemsComponents.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CartItemsService from '../../services/service-tksa/CartItemsService';
 import Card from "react-bootstrap/Card";
-import { Button, CardImg} from 'react-bootstrap';
+import { Button, CardImg, Table} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class CartItemsComponent extends Component {
@@ -39,52 +39,54 @@ class CartItemsComponent extends Component {
             <div>
                 <div className='a'>
                 <h2 className='text-center'>CART ITEMS</h2>
-                        
+
                 <div className='row'>
                     <div className='sddivcolour'>
-                    <table className='tablesize'>
-                    <br/> <br/> 
-                        
-                        <tr> 
-                            <td className='td_size'>
-                            {
-                                this.state.cart_item.map(
-                                    cartitem =>
-                                        <Card border="secondary" key = {cartitem.id} >
-                                            <Card.Header as="h3">{cartitem.itmeName}</Card.Header>
-                                            <Card.Body>
-                                                <table className='tablealign'>
-                                                    <tr>
-                                                        <td className='imageAlign'>
-                                                            <Card.Img />
-                                                        </td>
-                                                        <td className='colunmSize'>
-                                                            <Card.Text className='textAlign' >ITEM DESCRIPTION : {cartitem.itemDescription}</Card.Text>
-                                                            <Card.Text className='textAlign' >ITEM SIZE : {cartitem.itemSize}</Card.Text>
-                                                            <Card.Text className='textAlign' >ITEM COLOUR : {cartitem.itemColour}</Card.Text>
-                                                            <Card.Text className='textAlign' >ITEM PRICE : {cartitem.itemPrice}</Card.Text>
-                                                        </td>
-                                                        <td className='buttonAlign'>
-                                                                {/* <Button variant="danger"
-                                                                    onClick={() => this.deleteCartItem(cartitem.id)}
-                                                                    className = "btn btn-danger">
-                                                                        DELETE</Button> <br/> */}
-                                                                        <i class="fa fa-trash fa-2x" aria-hidden="true" onClick={() => this.deleteCartItem(cartitem.id)}></i>
-                                                        </td>
-                                                    </tr>
-                                                </table>
-                                               
-                                            </Card.Body>
-                                        </Card> 
-                                )
-                            }  <br/> <br/>
-                            </td> 
-                            <td>
-                               
-                                <div className= "container">
-                                    <div className= "row">
-                                        <div className= "card col-md-6 offset-md-3 offset-md-3"><br/>
-                                            <h6 className= "text-center"> ORDER SUMMARY </h6>
+                        <table className='tablesize'>
+                            <tr>
+                                <td>
+                                    <Table striped bordered hover size="sm"  className='shipping-d-div'>
+                                        <tbody>
+                                            <tr>
+                                            {
+                                                    this.state.cart_item.map(
+                                                        cartitem =>
+                                                            <Card border="secondary" key = {cartitem.id} >
+                                                                <Card.Header as="h3">{cartitem.itmeName}</Card.Header>
+                                                                <Card.Body>
+                                                                    <table className='tablealign'>
+                                                                        <tr>
+                                                                            <td className='imageAlign'>
+                                                                                <Card.Img />
+                                                                            </td>
+                                                                            <td className='colunmSize'>
+                                                                                <Card.Text className='textAlign' >ITEM DESCRIPTION : {cartitem.itemDescription}</Card.Text>
+                                                                                <Card.Text className='textAlign' >ITEM SIZE : {cartitem.itemSize}</Card.Text>
+                                                                                <Card.Text className='textAlign' >ITEM COLOUR : {cartitem.itemColour}</Card.Text>
+                                                                                <Card.Text className='textAlign' >ITEM PRICE : {cartitem.itemPrice}</Card.Text>
+                                                                            </td>
+                                                                            <td className='buttonAlign'>
+                                                                                    {/* <Button variant="danger"
+                                                                                        onClick={() => this.deleteCartItem(cartitem.id)}
+                                                                                        className = "btn btn-danger">
+                                                                                            DELETE</Button> <br/> */}
+                                                                                            <i class="fa fa-trash fa-2x" aria-hidden="true" onClick={() => this.deleteCartItem(cartitem.id)}></i>
+                                                                            </td>
+                                                                        </tr>
+                                                                    </table>
+                                                                
+                                                                </Card.Body>
+                                                            </Card> 
+                                                    )
+                                                }
+                                            </tr>
+                                        </tbody>
+                                    </Table>
+                                </td>
+
+                                <td>
+                                    <div className= "card  offset-md-3 offset-md-3"><br/>
+                                        <h5 className= "text-center"> ORDER SUMMARY </h5>
 
                                             <div className= "card-body">
                                                 <form>
@@ -93,9 +95,9 @@ class CartItemsComponent extends Component {
                                                         <input 
                                                             placeholder=' SUB TOTAL'
                                                             name='sub-total'
-                                                            className='form-control'
-                                                            />
+                                                            className='form-control'/>
                                                     </div><br/>
+
                                                     <div className="form-group">
                                                         <label> SHIPPING FEE </label>
                                                         <input 
@@ -103,6 +105,7 @@ class CartItemsComponent extends Component {
                                                             name='shipping-fee'
                                                             className='form-control'/>
                                                     </div><br/>
+                                                            
                                                     <div className="form-group">
                                                         <label> TOTAL PRICE </label>
                                                         <input 
@@ -111,21 +114,18 @@ class CartItemsComponent extends Component {
                                                             className='form-control'/>
                                                     </div><br/>
 
-                                                    <div className='buttonCenter'>
-                                                        <button type="button" className='button' class="btn btn-success" onClick={this.shippingDetails}>PAY NOW</button>
+                                                    <div className='buttonAlign'>
+                                                        <button class="btn btn-success" onClick={this.shippingDetails}>PAY NOW</button>
                                                     </div>
                                                 </form>
                                             </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
 
-                            
-                        </tr>
-                    </table>
+                                    </div>
+                                </td>
+                                
+                            </tr>
+                        </table>
                     </div>
-                      
                 </div>
 
                 </div>
